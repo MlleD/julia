ad61e25dcc08dad434c698de1b61b51f328c44a9
AdrienCavalieri <adr.cavalieri@gmail.com>
2018-11-20 06:33:44 +0100

    Ajout des classes Complex, DrawZone et MandelbrotWindow

---
+++ Complex.java
@@ -0,0 +1,39 @@
+public final class Complex 
+{
+	private double real;
+	private double imaginary;
+	
+	public Complex (double real, double imaginary)
+	{
+		this.real = real;
+		this.imaginary = imaginary;
+	}
+	
+	public double getReal()
+	{
+		return this.real;
+	}
+	
+	public double getImaginary()
+	{
+		return this.imaginary;
+	}
+	
+	public Complex plus(Complex other) 
+	{
+        double r = this.real + other.getReal();
+        double i = this.imaginary + other.getImaginary();
+        
+        return new Complex(r, i);
+    }
+	
+	public Complex times(Complex other)
+	{
+		double r = this.real * other.getReal() - this.imaginary * other.getImaginary();
+        double i = this.real * other.getImaginary() + this.imaginary * other.getReal();
+        
+        return new Complex(r, i);
+	}
+	
+
+}
---
+++ MandelbrotWindow.java
@@ -0,0 +1,74 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.JTextField;
+
+public class MandelbrotWindow extends JFrame
+{
+	private JTextField tX;
+	private JTextField tY;
+	private DrawZone drawZone;
+
+	public MandelbrotWindow(String titre, int w, int h)
+	{
+		super(title);
+		this.initializeMenu();
+		this.initialize();
+		this.setSize(w, h);
+		this.setVisible(true);	
+	}
+	
+	private void initialize()
+	{		
+		Container c = this.getContentPane();
+		
+		JPanel pNorth = this.createNorthPanel();
+		c.add(pNorth, BorderLayout.NORTH);	
+		
+		JPanel pDrawZone = this.createDrawZone();
+		c.add(pDrawZone, BorderLayout.CENTER);			
+	}
+	
+	private void initializeMenu()
+	{
+		JMenuBar menuBar = new JMenuBar();
+		this.setJMenuBar(menuBar);
+		
+		JMenu menuFichier = new JMenu("File");
+		menuBar.add(fileMenu);
+	}
+	
+	
+	private JPanel createDrawZone()
+	{
+		JPanel pDrawZone = new JPanel(new BorderLayout());
+		
+		return pDrawZone;
+	}
+	
+	private JPanel createNorthPanel()
+	{
+		JPanel pNorth = new JPanel();
+		
+		return pNorth;
+	}
+
+	class ActionL implements ActionListener
+	{
+		
+	}
+}
