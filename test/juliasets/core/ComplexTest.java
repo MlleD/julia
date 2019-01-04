2d47284082f42a07a0afd0761d4ae22cc9e08f10
AdrienCavalieri <adr.cavalieri@gmail.com>
2019-01-04 18:01:08 +0100

    Ajout de dossiers et fichiers nécessaires à la compilation

---
+++ test/juliasets/core/ComplexTest.java
@@ -0,0 +1,59 @@
+package juliasets.core;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ComplexTest {
+
+	@Test
+	void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testComplexDoubleDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testComplex() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetReal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetImaginary() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPlus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testTimes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testModulus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+}
